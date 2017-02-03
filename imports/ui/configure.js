498248f2dec2cb102385bd2181143661c7450689
Mark Watkins <mnmwatkins@gmail.com>
2017-02-02 19:52:01 -0700

    + Starting the configure pages.

--- imports/ui/configure.js
+++ imports/ui/configure.js
@@ -10,3 +10,29 @@ Template.configure.onCreated(function taskListOnCreated() {
     this.state = new ReactiveDict();
     Meteor.subscribe('elements');
 })
+
+Template.configure.events({
+    'submit .new-task'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Meteor.call('tasks.insert', text);
+
+        // Clear form
+        target.text.value = '';
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
+    'change .toggleOutlet'(event) {
+        Meteor.call('mqtt.send',event.target.id,event.target.checked);
+    },
+    ''(event) {
+        console.log(event);
+    }
+})
